08cc1d364aec0b43c2bf0662ec7d28d9da3a2139
Joseph Eagar <joeedh@gmail.com>
2020-05-16 09:16:56 -0700

    forgot these files

---
+++ example/electron_app.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      sandbox : false,
+      enableRemoteModule : true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('electron_app.html');
+
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow)
