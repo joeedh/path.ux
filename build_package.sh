15012459ebf356a6cabc44a6707ea493d9b2d324
Joseph Eagar <joeedh@gmail.com>
2020-05-20 19:43:05 -0700

    Move some files around

--- build_package.sh
+++ build_package.sh
@@ -12,7 +12,10 @@ cd _package
 cp ../pathux.tar .
 tar -xf pathux.tar
 
-cp -r scripts *.png *.js *.json docs dist example index.html *.pem *.MD watch.py render_icons.py ../package
+cp -r scripts *.png *.js *.json dist example index.html *.pem *.MD watch.py render_icons.py ../package
+rm -rf ../package/docs
+cp -r docs_src ../package/docs
+rm ../package/docs/.esdoc.json
 
 cd ..
 rm -rf _package
