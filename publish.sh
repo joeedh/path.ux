981844fee02aa101ace8f9625bb0eb522b57b4cb
Joseph Eagar <joeedh@gmail.com>
2020-09-30 16:43:14 -0700

    make publish.sh a bit more robust

--- publish.sh
+++ publish.sh
@@ -3,13 +3,14 @@
 VERSION=`cat package.json | grep version | sed 's/[" :,]//g' | sed 's/version//'`
 
 git commit -a
+git pull
 git push
 
 echo Publishing $VERSION
 bash build.sh && \
 #bash build_docs.sh && \
 bash build_package.sh && \
-cd package && \
+cd package && git commit -a && git push && \
 npm publish
 
 git tag -a $VERSION -m "Release $VERSION"
