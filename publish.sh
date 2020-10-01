f879a80538a2ac9d70a3148929431aca5e77bde2
Joseph Eagar <joeedh@gmail.com>
2020-09-30 23:45:07 -0700

    i

--- publish.sh
+++ publish.sh
@@ -3,14 +3,32 @@
 VERSION=`cat package.json | grep version | sed 's/[" :,]//g' | sed 's/version//'`
 
 git commit -a
+echo $?
 git pull
+echo $?
 git push
+echo $?
 
 echo Publishing $VERSION
-bash build.sh && \
-#bash build_docs.sh && \
-bash build_package.sh && \
-cd package && git commit -a && git push && \
-echo "npm publish" && npm publish
-
 echo $?
+bash build.sh
+echo $?
+
+#bash build_docs.sh && \
+bash build_package.sh
+echo $?
+
+cd package
+echo $?
+
+git commit -a
+echo $?
+git push
+echo $?
+
+echo "npm publish"
+echo $?
+npm publish
+echo $?
+
+#git tag -a $VERSION -m "Release $VERSION"
