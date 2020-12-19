9ac7003a7e406e35003addfcc26337292fc12110
Joseph Eagar <joeedh@gmail.com>
2020-12-18 17:46:19 -0800

    Add missing export forward

--- scripts/pathux.js
+++ scripts/pathux.js
@@ -21,6 +21,7 @@ export * from './widgets/ui_lasttool.js';
 export * from './widgets/ui_textbox.js';
 export * from './path-controller/util/graphpack.js';
 
+export * from './path-controller/util/html5_fileapi.js';
 export * from './path-controller/controller.js';
 
 import * as controller1 from './path-controller/controller.js';
