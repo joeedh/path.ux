c2dd27371a6a6744c4bce83064ca0a005f564cd9
Joseph Eagar <joeedh@gmail.com>
2023-11-25 15:44:35 -0800

    Update typescript types

---
+++ scripts/types/core/aspect.d.ts
@@ -0,0 +1,5 @@
+export declare function clearAspectCallbacks(obj: any): void;
+
+export declare function initAspectClass(objct: any, blacklist: Set<any>): void;
+
+export declare function _setUIBase(UIBase: any): void;
---
+++ scripts/types/pathux.d.ts
@@ -0,0 +1,4 @@
+export * from './core/ui_base';
+export * from './core_ui';
+export * from '../path-controller/types/controller.d.ts'
+
