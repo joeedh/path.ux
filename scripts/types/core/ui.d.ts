ae912a6372404840500f56f44865cdb009845b1b
Joseph Eagar <joeedh@gmail.com>
2023-12-02 22:08:03 -0800

    Update typescript types

--- scripts/types/core/ui.d.ts
+++ scripts/types/core/ui.d.ts
@@ -3,47 +3,58 @@ export as namespace ui;
 import {UIBase, pathUXInt} from './ui_base';
 
 interface ListEnumArgs {
-    mass_set_path? : string;
-    enumDef? : string;
-    name? : string;
-    callback? : Function;
-    iconmap? : object;
-    packflag? : pathUXInt;
-    defaultval : number|string;
+  mass_set_path?: string;
+  enumDef?: string;
+  name?: string;
+  callback?: Function;
+  iconmap?: object;
+  packflag?: pathUXInt;
+  defaultval: number | string;
 }
 
 interface SliderArgs {
-    mass_set_path? : string;
-    enumDef? : string;
-    name? : string;
-    callback? : Function;
-    iconmap? : object;
-    packflag? : pathUXInt;
-    min : number;
-    max : number;
-    decimals : pathUXInt;
-    step : number;
-    is_int : boolean;
-    do_redraw : boolean;
-    defaultval : number;
+  mass_set_path?: string;
+  enumDef?: string;
+  name?: string;
+  callback?: Function;
+  iconmap?: object;
+  packflag?: pathUXInt;
+  min: number;
+  max: number;
+  decimals: pathUXInt;
+  step: number;
+  is_int: boolean;
+  do_redraw: boolean;
+  defaultval: number;
 }
 
 declare class Container extends UIBase {
-    prop(path : string, packflag : pathUXInt) : UIBase;
-    prop(path : string) : UIBase;
-    row(packflag : pathUXInt) : RowFrame;
-    col(packflag : pathUXInt) : ColumnFrame;
-    label(str : string) : UIBase;
-    table() : TableFrame;
-    listenum(inpath : string, name : string, enumDef : object, defaultval? : any, callback? : Function, iconmap? : object, packflag? : pathUXInt): UIBase;
-    listenum(inpath : string, args? : ListEnumArgs): UIBase;
-    slider(inpath : string, name : string, defaultval : number, min : number, max : number, step : number, is_int : boolean, do_redraw : boolean, callback : Function, packflag : number): UIBase;
-    slider(inpath : string, args : SliderArgs): UIBase;
+  prop(path: string, packflag: pathUXInt): UIBase;
+  prop(path: string): UIBase;
+
+  row(packflag: pathUXInt): RowFrame;
+
+  col(packflag: pathUXInt): ColumnFrame;
+
+  label(str: string): UIBase;
+
+  colorbutton(str: string): UIBase;
+
+  vecpopup(path: string): UIBase;
+
+  table(): TableFrame;
+
+  listenum(inpath: string, name: string, enumDef: object, defaultval?: any, callback?: Function, iconmap?: object, packflag?: pathUXInt): UIBase;
+  listenum(inpath: string, args?: ListEnumArgs): UIBase;
+
+  slider(inpath: string, name: string, defaultval: number, min: number, max: number, step: number, is_int: boolean, do_redraw: boolean, callback: Function, packflag: number): UIBase;
+  slider(inpath: string, args: SliderArgs): UIBase;
 }
 
 declare class TableFrame extends Container {
 
 }
+
 declare class RowFrame extends Container {
 
 }
--- scripts/types/pathux.d.ts
+++ scripts/types/pathux.d.ts
@@ -2,3 +2,5 @@ export * from './core/ui_base';
 export * from './core_ui';
 export * from '../path-controller/types/controller.d.ts'
 
+export const keymap: { [k: string]: number };
+
