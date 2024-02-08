a290490353dfc796712406b9b834856a937ada5d
Joe Eagar <joeedh@gmail.com>
2024-02-08 11:20:58 -0800

    add missing file

---
+++ scripts/types/simple/simple.d.ts
@@ -0,0 +1,6 @@
+import { DataStruct } from "../../path-controller/types/controller/controller_base";
+
+export declare class DataModel {
+  static defineAPI(api, dataStruct): DataStruct;
+  static register(Class: any);
+}
