8507742be674eb89b140f5750baf833e87c560dc
Joe Eagar <joeedh@gmail.com>
2024-02-08 11:49:00 -0800

    update simple.d.ts

--- scripts/types/simple/simple.d.ts
+++ scripts/types/simple/simple.d.ts
@@ -1,6 +1,11 @@
 import { DataStruct } from "../../path-controller/types/controller/controller_base";
+import { Area } from "../screen/ScreenArea";
 
 export declare class DataModel {
   static defineAPI(api, dataStruct): DataStruct;
   static register(Class: any);
+}
+
+export class Editor extends Area {
+  static findEditor(Class: any): boolean;
 }
